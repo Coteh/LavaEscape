3fe1174b0d9adf35393ec93aedf825c665df72ae
Coteh <3276350+Coteh@users.noreply.github.com>
2019-12-31 15:50:00 -0500

    Add placeholder background to title screen

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -1,4 +1,7 @@
 import { BaseButton } from "../gameobjects/buttons/BaseButton";
+import { Lava } from "../gameobjects/Lava";
+import { AbstractChunkFactory } from "../factory/chunks/AbstractChunkFactory";
+import { Pickup } from "../gameobjects/Pickup";
 
 export class TitleScene extends Phaser.Scene {
     private menuButtons: BaseButton[];
@@ -12,6 +15,10 @@ export class TitleScene extends Phaser.Scene {
         this.menuButtons = [];
     }
 
+    preload(): void {
+        this.load.image("reg_platform", "./assets/img/Platform.png");
+    }
+
     create(): void {
         var startBtn: BaseButton = new BaseButton(this, this.cameras.main.centerX - 60, this.cameras.main.centerY, "Start Game", this.startGame.bind(this));
         this.add.existing(startBtn);
@@ -21,6 +28,19 @@ export class TitleScene extends Phaser.Scene {
         });
         this.copyright = this.add.text(this.cameras.main.centerX - 60 - 40, this.cameras.main.height - 32, String.fromCharCode(169) + " 2019 James Cote", {
             color: "#fff"
+        });
+        var lava = new Lava(this, 800, null, null);
+        this.add.existing(lava);
+        lava.setDepth(-500);
+        var factory = new AbstractChunkFactory(this, null, null, null);
+        var chunk = factory.createChunk(-200, 500);
+        chunk.blocks.forEach(block => {
+            this.add.existing(block);
+            block.setDepth(-300);
+        });
+        chunk.pickups.forEach(pickup => {
+            this.add.existing(pickup);
+            pickup.setDepth(-300);
         });
     }
 
