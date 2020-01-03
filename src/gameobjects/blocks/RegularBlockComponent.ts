9f53ce0e0756ad9967d13145626dcb5920e635d3
Coteh <3276350+Coteh@users.noreply.github.com>
2020-01-02 20:47:40 -0500

    Fix scene undefined issue if player is destroyed before lock on

--- src/gameobjects/blocks/RegularBlockComponent.ts
+++ src/gameobjects/blocks/RegularBlockComponent.ts
@@ -7,7 +7,9 @@ export class RegularBlockComponent extends BlockComponent {
     }
 
     public runEffect() {
-        this.player.lockOn(this.block);
-        this.player.jump(this.block.getJumpFactor());
+        if (this.player.active) {
+            this.player.jump(this.block.getJumpFactor());
+            this.player.lockOn(this.block);
+        }
     }
 }
